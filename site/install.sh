a21ad00912c44cc0041eef8d2ce74f0f85087378
pejman hkh <pejman.hkh@gmail.com>
2023-12-11 13:50:35 +0330

    e

--- site/install.sh
+++ site/install.sh
@@ -2,7 +2,9 @@ chmod 0777 -R home/pma/tmp/
 read -p 'Username: ' user
 read -p 'Domain without www: ' domain
 echo $user : $domain;
-mkdir -p home/$user/$domain/public_html
+(cd home/$user/$domain/ && ln -s $user/public public_html)
+
+mkdir -p home/$user/$domain
 mkdir -p home/$user/tmp
 cp php81/sample/partakbi.ir.conf php81/php-fpm.d/$domain.conf
 sed -i 's\partak\'"$user"'\g' php81/php-fpm.d/$domain.conf
@@ -11,6 +13,11 @@ sed -i 's\partakbi.ir\'"$domain"'\g' nginx/http.d/$domain.conf
 sed -i 's\partak\'"$user"'\g' nginx/http.d/$domain.conf
 
 mkdir -p letsencrypt/live/$domain/
+
+read -p 'Enter repository url: ' rep
+mkdir home/$user/$domain/$user
+git clone $rep home/$user/$domain/$user/.
+sudo docker compose exec nginx sh -c "cd /home/$user/$domain/$user/ && bash install.sh"
 
 cp data/certs/domain.ir.cert letsencrypt/live/$domain/fullchain.pem;
 cp data/certs/domain.ir.key letsencrypt/live/$domain/privkey.pem;

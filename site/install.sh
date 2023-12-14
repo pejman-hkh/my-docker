78a4469bc91eaa8065925a0e5f163ba8ad53c31d
pejman <pejman.hkh@gmail.com>
2023-12-14 14:18:52 +0330

    e

--- site/install.sh
+++ site/install.sh
@@ -5,11 +5,11 @@ echo $user : $domain;
 mkdir -p home/$user/$domain;
 (cd home/$user/$domain/ && ln -s $user/public public_html)
 mkdir -p home/$user/tmp
-cp php81/sample/partakbi.ir.conf php81/php-fpm.d/$domain.conf
-sed -i 's\partak\'"$user"'\g' php81/php-fpm.d/$domain.conf
-cp nginx/sample/partakbi.ir.conf nginx/http.d/$domain.conf
-sed -i 's\partakbi.ir\'"$domain"'\g' nginx/http.d/$domain.conf
-sed -i 's\partak\'"$user"'\g' nginx/http.d/$domain.conf
+cp php81/sample/domain.ir.conf php81/php-fpm.d/$domain.conf
+sed -i 's\usersample\'"$user"'\g' php81/php-fpm.d/$domain.conf
+cp nginx/sample/domain.ir.conf nginx/http.d/$domain.conf
+sed -i 's\domain.ir\'"$domain"'\g' nginx/http.d/$domain.conf
+sed -i 's\usersample\'"$user"'\g' nginx/http.d/$domain.conf
 
 mkdir -p letsencrypt/live/$domain/
 
